397bb2b23d15d4ce1d2c54c27e6bdca98a3da07c
mazefernandez <aafernandez5@up.edu.ph>
2019-03-05 08:14:31 +0000

    added item controller and endpoints

---
+++ server/controllers/item.js
@@ -0,0 +1,95 @@
+'use strict' 
+
+const mysql = require('mysql') 
+const connection = require(__dirname + '/../db.js')
+
+exports.getItems = (req,res) => {
+    connection.query('SELECT * FROM Item', [], function(err, rows, fields) {
+    	if (!err) {
+	    res.send(rows)
+	    console.log("Retrieved all items")
+	}
+	else {
+	    res.send(err)
+	    console.log("Error in retrieving all items " + err)
+	}
+    })
+}
+
+exports.getItemsFromCustomer = (req, res) => {
+    connection.query('SELECT * FROM Item where customer_id = ?', [req.params.id], function (err, rows, fields) {
+        if (!err) {
+	   res.send(rows)
+	   console.log("Retrieved all items from customer")
+	}
+	else {
+	    res.send(err)
+	    console.log("Error in retrieving all items from customer " + err)
+	}
+    })
+}
+
+exports.getItem = (req,res) => {
+    connection.query('SELECT * FROM Item where item_id = ?', [req.params.id], function(err, rows, fields) {
+	if (!err) {
+	    res.send(rows[0])
+	    console.log("Error in retrieving item " + err)
+	}
+    })
+}
+
+exports.addItem = (req,res) => {
+    var item = {
+	name : req.body.name, 
+	description : req.body.description,
+	price : req.body.price,
+	image : req.body.image,
+	condition : req.body.condition,
+	customer_id : req.body.customer_id
+    }
+    connection.query('INSERT into Item SET ?', item, function(err, rows, fields) {
+	if (!err) {
+	    item.item_id = rows.insertId
+	    res.send(item)
+	    console.log("Added new item")
+	}
+	else {
+	    res.send(err)
+	    console.log("Error in adding new item " + err)
+	}
+    }) 
+}
+
+exports.updateItem = (req,res) => {
+    var item = {
+	name : req.body.name,
+	description : req.body.description,
+	price : req.body.price,
+	image : req.body.image,
+	condition : req.body.condition
+    }
+    connection.query('UPDATE Item SET name = ?, description = ?, price = ?, image = ?, condition = ? where item_id = ?', [req.body.name, req.body.description, req.body.price, req.body.image, req.body.condition, req.params.id], function(err, rows, fields) {
+        if (!err) {
+	    res.send(item)
+	    console.log("Updated item")
+	}
+	else {
+	    res.send(err)
+	    console.log("Error in updating item " + err) 
+	}
+    })
+}
+
+exports.deleteItem = (req,res) => {
+    connection.query('DELETE FROM Item WHERE item_id = ?', [req.params.id], function(err, rows, fields) {
+	if (!err) {
+	    res.send(null)
+	    console.log("Deleted item")
+	}
+	else {
+	    res.send(err)
+	    console.log("Error in deleting item " + err)
+	}
+    })
+}
+
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,11 +4,20 @@ var express = require('express')
 var router = express.Router()
 
 var customerCtrl = require('../controllers/customer.js')
+var itemCtrl = require('../controllers/item.js')
 
 //routes for customers
 router.get('/api/customers', customerCtrl.getCustomers)
 router.get('/api/customers/:id', customerCtrl.getCustomer)
 router.post('/api/customers', customerCtrl.addCustomer)
 router.put('/api/customers/:id', customerCtrl.updateCustomer)
+
+//routes for items
+router.get('api/items', itemCtrl.getItems)
+router.get('api/items/:id', itemCtrl.getItemsFromCustomer)
+router.get('api/items/:id', itemCtrl.getItem)
+router.post('api/items', itemCtrl.addItem)
+router.put('api/items/:id', itemCtrl.updateItem)
+router.delete('api/items/:id', itemCtrl.deleteItem)
 
 module.exports = router;
