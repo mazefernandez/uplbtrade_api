85e3fe0b2e34b907df0e032c4d242cfcc5c0a477
mazefernandez <aafernandez5@up.edu.ph>
2019-03-08 10:13:04 +0000

    add offer controller and routes

---
+++ server/controllers/offer.js
@@ -0,0 +1,65 @@
+'use strict' 
+
+const mysql = require('mysql') 
+const connection = require(__dirname + '/../db.js'
+
+exports.getOffers = (req,res) => {
+    connection.query('SELECT * FROM Offer', [], function (err, rows, fields) {
+	if (!err) {
+	    res.send(rows)
+	    console.log("Retrieved all offers") 
+	}
+	else {
+	    res.send(err)
+	    console.log("Error in retrieving all offers " + err)
+	}
+    })
+}
+
+exports.getOffer = (req,res) => {
+    connection.query('SELECT * FROM Offer where offer_id = ?', [req.params.id], function(err, rows, fields {
+    	if (!err) {
+	    res.send(rows[0])
+	    console.log("Retrieved offer")
+   	 }
+    	else {
+	    res.send(err)
+	    console.log("Error in retrieving offer") 
+    	}
+    })
+}
+
+exports.addOffer = (req,res) => {
+    var offer = {
+	price : req.body.price, 
+	status : req.body.status,
+        message : req.body.message, 
+	item_id : req.body.item_id,
+	buyer_id : req.body.buyer_id,
+	seller_id : req.body.seller_id 
+    }
+    connection.query('INSERT INTO Offer SET ?', offer, function(err, rows, fields) {
+        if (!err) {
+	    offer.offer_id = rows.insertId
+	    res.send(offer)
+	    console.log("Added new offer")
+	}
+	else {
+	    res.send(err)
+	    console.log("Error in adding new offer " + err)
+	}
+    }) 
+}	    
+
+exports.deleteOffer = (req,res) => {
+    connection.query('DELETE FROM Offer WHERE offer_id = ?', [req.params.id], function(err, rows, fields) {
+	if (!err) {
+	    res.send(null)
+	    console.log("Deleted offer")
+	}
+	else {
+	    res.send(err)
+	    console.log("Error in deleting offer " + err) 
+	}
+    )}
+}
