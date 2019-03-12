762ec2f4bc9ea1cb1d26bd8e7e4db943b3a18b59
mazefernandez <aafernandez5@up.edu.ph>
2019-03-12 15:14:33 +0000

    Fix delete in offer

--- server/controllers/offer.js
+++ server/controllers/offer.js
@@ -90,3 +90,4 @@ exports.deleteOffer = (req,res) => {
 	}
     })
 }
+
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -29,7 +29,7 @@ router.get('/api/items/search/:search', itemCtrl.searchItems)
 router.get('/api/offers', offerCtrl.getOffers)
 router.get('/api/offers/:id', offerCtrl.getOffer)
 router.post('/api/offers', offerCtrl.addOffer)
-router.delete('api/offers/:id', offerCtrl.deleteOffer)
+router.delete('/api/offers/:id', offerCtrl.deleteOffer)
 
 router.get('/api/offers/buyer/:id', offerCtrl.getOfferBuying)
 router.get('/api/offers/seller/:id', offerCtrl.getOfferSelling)
