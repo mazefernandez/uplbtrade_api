f91dcd4d02c994be97ae4c008022b26f8787f232
mazefernandez <aafernandez5@up.edu.ph>
2019-04-16 16:06:55 +0800

    Add routes

--- client/controllers/indexCtrl.js
+++ client/controllers/indexCtrl.js
@@ -5,22 +5,23 @@ admin.controller('indexCtrl', function($scope) {
 	// specify base tag for href
 });
 
-// admin.config(function ($routeProvider, $locationProvider) {
-// 	$locationProvider.html5Mode(true);
-// 	$routeProvider.when('/', {
-// 		templateUrl : '../index.html',
-// 		controller : 'indexCtrl'
-// 	}).when('/reviews', {
-// 		templateUrl : '../index.html',
-// 		controller : 'indexCtrl'
-// 	}).when('/reports', {
-// 		templateUrl : '../reports.html',
-// 		controller : 'indexCtrl'
-// 	}).when('/history', {
-// 		templateUrl : '../transacion-logs.html',
-// 		controller : 'indexCtrl'
-// 	}).when('/database', {
-// 		templateUrl : '../database.html',
-// 		controller : 'indexCtrl'
-// 	});
-// });
+admin.config(['$routeProvider', function ($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl : 'index.html',
+		controller : 'indexCtrl'
+	}).when('/reviews', {
+		templateUrl : 'index.html',
+		controller : 'indexCtrl'
+	}).when('/reports', {
+		templateUrl : 'reports.html',
+		controller : 'indexCtrl'
+	}).when('/history', {
+		templateUrl : 'transacion-logs.html',
+		controller : 'indexCtrl'
+	}).when('/database', {
+		templateUrl : 'database.html',
+		controller : 'indexCtrl'
+	}).otherwise({
+		redirectTo : 'index.html'
+	});
+}]);
