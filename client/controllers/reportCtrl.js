cbb79c0a9f2c9386c174529f5141d0c611fe9518
mazefernandez <aafernandez5@up.edu.ph>
2019-04-29 00:54:39 +0800

    Add Report Controller

--- client/controllers/reportCtrl.js
+++ client/controllers/reportCtrl.js
@@ -2,7 +2,31 @@ var admin = angular.module('admin');
 
 admin.controller('reportCtrl', ['$scope', '$http', function($scope, $http) {
 
-	$http.get('').then{}
+	$http.get('/api/customer-reports').then(function(response) {
+		var retrieved = response.data; 
+		var reports = [];
 
+		retrieved.forEach(x) {
+			var rep, cus; 
+
+			$http.get('api/customers', x.customer_id).then(function(response) {
+				cus = response.data; 
+			})
+
+			$http.get('api/customers', x.reporter_id).then(function(response) {
+				rep = response.data; 
+			})
+
+			reports.add({
+				report : x, 
+				customer : cus, 
+				reporter : rep
+			})
+		}
+
+		$scope.reports = reports; 
+		console.log(reports); 
+
+	})
 }]);
 
