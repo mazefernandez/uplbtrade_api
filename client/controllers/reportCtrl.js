9e7472d4684e123d62a1d8331becd2460d061045
mazefernandez <aafernandez5@up.edu.ph>
2019-04-29 01:42:38 +0800

    fix report controller

--- client/controllers/reportCtrl.js
+++ client/controllers/reportCtrl.js
@@ -1,32 +1,27 @@
 var admin = angular.module('admin');
 
 admin.controller('reportCtrl', ['$scope', '$http', function($scope, $http) {
-
 	$http.get('/api/customer-reports').then(function(response) {
 		var retrieved = response.data; 
 		var reports = [];
-
+		var r = {};
+		var c = {}
 		retrieved.forEach(function(x) {
-			var rep, cus;  
-
-			$http.get('/api/customers', {customer_id : x.reporter_id}).then(function(response) {
-				rep = response.data;
+			$http.get('/api/customers/' + x.reporter_id).then(function(response) {
+				r = response.data;
+				x.reporter = r;
 			})
 
-			$http.get('/api/customers', {customer_id : x.customer_id}).then(function(response) {
-				cus = response.data;
+			$http.get('/api/customers/' + x.customer_id).then(function(response) {
+				c = response.data;
+				x.customer = c;
 			})
-
-			reports.push({
-				report : x, 
-				reporter : rep,
-				customer : cus
-			})	
+			reports.push(x)
 		})
 
 		$scope.reports = reports; 
-		console.log(reports); 
-
+		console.log($scope.reports);
 	})
 }]);
+
 
