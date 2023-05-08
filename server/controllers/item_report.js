e9ce4e8fe72a79f0bcec9d5e1f19e7814880c16f
mazefernandez <mazefernandez@github.com>
2023-05-08 14:45:17 +0000

    added controller and route for item_report

---
+++ server/controllers/item_report.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const connection = require(__dirname + '/../db.js')
+
+exports.addItemReport = (req,res) => {
+	var item_report = {
+		message : req.body.message,
+		date : require('moment')().format('YYYY-MM-DD HH:mm:ss'),
+		reporter_id : req.body.reporter_id,
+		item_id : req.body.item_id
+	}
+	connection.query('INSERT INTO Item_Report SET ?', item_report, function(err, rows, fields){
+		if (!err) {
+			item_report.report_id = rows.insertId;
+			res.send(item_report)
+			console.log("Retrieved item report")
+		}
+		else {
+			res.send(err)
+			console.log("Failed to retrieve item report " + err)
+		}
+	})
+}
+
+exports.getItemReports = (req,res) => {
+	connection.query('SELECT * FROM Item_Report', [], function(err, rows, fields){
+		if (!err) {
+			res.send(rows)
+			console.log("Retrieved item reports")
+		}
+		else {
+			res.send(err)
+			console.log("Error in retrieving item reports " + err)
+		}
+	})
+}
